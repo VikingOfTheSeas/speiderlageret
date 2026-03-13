7bec1160a634c9ccdc949da287f5cd7b571d818c
VikingOfTheSeas <159448020+VikingOfTheSeas@users.noreply.github.com>
2026-03-13 10:19:51 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL      = 'https://mxnojoymdgeapwsljkbg.supabase.co';
-const SUPABASE_ANON_KEY = 'LIMPINN_DIN_NOKKEL_HER';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im14bm9qb3ltZGdlYXB3c2xqa2JnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMzMTczNzgsImV4cCI6MjA4ODg5MzM3OH0.IZuE0og-G-12Izu2VVyx_kV8sDHUE6nG5Ziucdi-FNk';
 const APP_BASE_URL      = 'https://1-haugerud-lager-administrasjon.vercel.app/';
 
 const { createClient } = window.supabase;
